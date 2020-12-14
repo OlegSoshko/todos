e8c0d9b1e49f3e830d9b60d9093939a8fc17b9b8
Oleg Soshko <oleg.91.03@gmail.com>
2020-12-14 16:20:34 +0300

    modified url list

--- src/utils/urlList.ts
+++ src/utils/urlList.ts
@@ -1,6 +1,5 @@
 const URL = {
-    GET_TODOS: 'https://5fd482c5e9cda40016f5c1bf.mockapi.io/todoapp/todos',
-    DELETE_TODO: 'https://5fd482c5e9cda40016f5c1bf.mockapi.io/todoapp/todos',
+    TODOS: 'https://5fd482c5e9cda40016f5c1bf.mockapi.io/todoapp/todos',
 }
 
 export default URL;
